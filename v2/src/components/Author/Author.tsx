e53c83412b186a7ddb448315867cb99102a08f3d
LordDashMe <reyesjoshuaclifford@gmail.com>
2019-09-02 22:53:39 +0800

    chore: ongoing development.

--- v2/src/components/Author/Author.tsx
+++ v2/src/components/Author/Author.tsx
@@ -12,10 +12,10 @@ export default class Author extends Component<IAuthorProperty, IAuthorState> {
     return (
       <div className="Author">
         <div className="author-img">
-          <img src="resources/img/profile-taiwan-tour.jpg" alt="Joshua Clifford Reyes Profile" />
+          <img src="resources/img/profile-shanghai.jpg" alt="Joshua Clifford Reyes Profile" />
         </div>
         <div className="author-description">
-          <p>I'm JOSHUA CLIFFORD REYES (or you can call me DASH), currently enjoying the fields or roles of</p>
+          {/* <p>I'm JOSHUA CLIFFORD REYES (or you can call me DASH), currently enjoying the fields or roles of</p> */}
           <p>INFORMATION TECHNOLOGY | WEB DEVELOPER | MOBILE DEVELOPER | DEVOPS | COMPUTER NETWORK | DATA STRUCTURE | MULTIMEDIA</p>
         </div>
       </div>
