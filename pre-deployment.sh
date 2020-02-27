3f77c739697818d3d43fcff7b4c0f31aa434b9f0
Joshua Clifford Reyes <reyesjoshuaclifford@gmail.com>
2020-02-27 10:16:51 +0800

    Build: except google verification file.

--- pre-deployment.sh
+++ pre-deployment.sh
@@ -8,6 +8,7 @@ find $(pwd)/* ! \( \
   -type d -name 'v3' -prune -o \
   -type f -name '.gitignore' -prune -o \
   -type f -name 'ads.txt' -prune -o \
+  -type f -name 'google764e0d137ba9e2a9.html' -prune -o \
   -type f -name 'LICENSE' -prune -o \
   -type f -name 'pre-deployment.sh' -prune -o \
   -type f -name 'README.md' -prune -o \
