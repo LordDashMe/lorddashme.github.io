cf0f4e72289cb1d00a39541572c275179d6b0229
LordDashMe <reyesjoshuaclifford@gmail.com>
2020-04-26 19:07:40 +0800

    build: boiler plate for status page.

---
+++ v3/src/pages/status/index.tsx
@@ -0,0 +1,11 @@
+import { useEffect } from 'react';
+import { navigate } from 'gatsby';
+
+export default () => {
+  
+  useEffect(() => {
+    navigate('/comingsoon');
+  }, []);
+
+  return null;
+};
